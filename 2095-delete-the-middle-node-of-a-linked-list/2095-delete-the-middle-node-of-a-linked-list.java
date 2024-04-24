a1fd37eab9a3570b1f548ef93216dc44ce65526e
nswetha25 <152351222+nswetha25@users.noreply.github.com>
2024-04-24 00:17:50 -0400

    Time: 3 ms (99.82%), Space: 64 MB (54.61%) - LeetHub

---
+++ 2095-delete-the-middle-node-of-a-linked-list/2095-delete-the-middle-node-of-a-linked-list.java
@@ -0,0 +1,67 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution {
+    //https://www.youtube.com/watch?v=IPaMfcxQtP0
+    public ListNode deleteMiddle(ListNode head) {
+
+//         if(head==null||head.next==null) return null;
+
+        
+
+//         int length=0;
+//         ListNode current=head;
+//         while (current!=null){
+
+//             length++;
+//             current=current.next;
+
+
+//         }
+//           System.out.println(length);
+//           //Resetting the curent to the head
+//           current=head;
+//           int removeindex= length/2;
+//            System.out.println(removeindex);
+// //Lets navigate to one node before the removal of the node
+//           for(int i=0;i<removeindex-1;i++){
+//             current = current.next;
+//             }
+
+//           //Lets skip that middle node
+//           current.next=current.next.next;
+
+
+
+          
+ 
+//         return head;
+
+//One more better way to remove the middle element is using tortoise and hare approach
+
+       if(head==null||head.next==null) return null;
+
+       ListNode slow=head;
+       ListNode fast=head;
+       ListNode prev=null;
+
+       while(fast!=null && fast.next!=null){
+        fast=fast.next.next;
+        prev=slow;
+        slow=slow.next;
+       }
+       
+
+       prev.next=slow.next;
+
+       return head;
+        
+    }
+    }
