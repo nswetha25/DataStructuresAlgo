136fa0ce619d5444cd3329c50804f2b2c5e4ea0a
nswetha25 <152351222+nswetha25@users.noreply.github.com>
2024-04-24 23:18:46 -0400

    Time: 4 ms (97.65%) | Memory: 63.5 MB (58.89%) - LeetSync

--- 2236-maximum-twin-sum-of-a-linked-list/maximum-twin-sum-of-a-linked-list.java
+++ 2236-maximum-twin-sum-of-a-linked-list/maximum-twin-sum-of-a-linked-list.java
@@ -14,27 +14,69 @@ class Solution {
 //but this is singly linkedlist
 
 //Lets use an array which occupies O(n) space
-        if(head==null) return 0;
+        // if(head==null) return 0;
 
-        if(head.next==null) return head.val;
+        // if(head.next==null) return head.val;
          
-        ArrayList<Integer> array=new ArrayList<>();
-        ListNode current=head;
-        while(current!=null){
-            array.add(current.val);
-            current=current.next;
+        // ArrayList<Integer> array=new ArrayList<>();
+        // ListNode current=head;
+        // while(current!=null){
+        //     array.add(current.val);
+        //     current=current.next;
 
-        }
+        // }
 
-        System.out.println(array);
-        int count=0;
-         int n=array.size();
-        for(int i=0;i<n;i++){
+        // System.out.println(array);
+        // int count=0;
+        //  int n=array.size();
+        // for(int i=0;i<n;i++){
           
-          if(array.get(i)+array.get(n-i-1)>count) { count=array.get(i)+array.get(n-i-1);}
+        //   if(array.get(i)+array.get(n-i-1)>count) { count=array.get(i)+array.get(n-i-1);}
+        // }
+
+        // return count;
+
+        //Above code works but its not efficient of having arraylist again
+        //Lets see another way
+
+        ListNode slow=head;
+        ListNode fast=head;
+        ListNode prev=null;
+
+        while(fast!=null && fast.next!=null){
+            prev=slow;
+            slow=slow.next;
+            fast=fast.next.next;
         }
+       
+       //Next reversing the second half
+       ListNode current=slow;
+       ListNode prevnode=null;
+
+       while(current!=null){
+        ListNode next= current.next;
+        current.next=prevnode;
+        prevnode=current;
+        current=next;
+
+       }
+
+       //calculating the max twinsum
+       int count=0;
+
+       ListNode firsthalf=head;
+       ListNode secondhalf=prevnode;
+
+       while(secondhalf!=null){
+
+        count=Math.max(count,firsthalf.val+secondhalf.val);
+        firsthalf=firsthalf.next;
+        secondhalf=secondhalf.next;
+       }
+
+
+
 
         return count;
-        
     }
 }
