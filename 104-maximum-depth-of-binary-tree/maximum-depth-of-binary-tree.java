eb16d79638fb4427e4454d4d4f8d75f051ecdd63
nswetha25 <152351222+nswetha25@users.noreply.github.com>
2024-04-27 10:19:44 -0400

    Time: 1 ms (10.82%) | Memory: 43.1 MB (8.51%) - LeetSync

--- 104-maximum-depth-of-binary-tree/maximum-depth-of-binary-tree.java
+++ 104-maximum-depth-of-binary-tree/maximum-depth-of-binary-tree.java
@@ -16,12 +16,51 @@
 class Solution {
     public int maxDepth(TreeNode root) {
 
-       if(root==null) return 0;
+//instead of trying recursive method lets try iterative method
+    //    if(root==null) return 0;
 
-       int left=maxDepth(root.left);
-       int right=maxDepth(root.right);
+    //    int left=maxDepth(root.left);
+    //    System.out.println("left"+left);
+    //    int right=maxDepth(root.right);
+    //    System.out.println("left"+left);
+    //    return 1+Math.max(left,right);
 
-       return 1+Math.max(left,right);
-        
+    //Lets try iterative method below: We can use either stack or queue depending on the scenario
+
+    //For iterative method we can use level order traversal to find the height of binary tree
+          
+          if(root==null) return 0;
+          //Lets create an empty queue for level order traversal
+
+          Queue<TreeNode> q=new LinkedList<>();
+
+          //First add the root node to queue and initialize height
+
+          q.add(root);
+          int depth=0;
+
+          while(!q.isEmpty()){
+            int size=q.size();
+
+            for(int i=0;i<size;i++){
+                TreeNode current=q.poll();
+
+            // Add children of the current node to the queue
+                if (current.left != null) {
+                    q.offer(current.left);
+                }
+                if (current.right != null) {
+                    q.offer(current.right);
+                }
+            }
+            depth++;
+          }
+          return depth;
     }
-}
+
+          }
+
+         
+
+        
+    
