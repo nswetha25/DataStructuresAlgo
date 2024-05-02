2acc0840971ebbceb3ab616e25cb07d11cad0587
nswetha25 <152351222+nswetha25@users.noreply.github.com>
2024-05-01 23:50:33 -0400

    Time: 61 ms (47.79%) | Memory: 61.7 MB (11.59%) - LeetSync

--- 215-kth-largest-element-in-an-array/kth-largest-element-in-an-array.java
+++ 215-kth-largest-element-in-an-array/kth-largest-element-in-an-array.java
@@ -2,12 +2,33 @@ class Solution {
     public int findKthLargest(int[] nums, int k) {
 
 
-       Arrays.sort(nums);
+    //    Arrays.sort(nums);
 
 
         
 
-        return nums[nums.length-k];
+    //     return nums[nums.length-k];
+
+    //Above method is correct but they said without sorting so lets see
+    //when we here about largest or smallest elements we want to use heap ds
+    //https://chat.openai.com/c/df825d62-c478-4ad8-9b29-70bda1abc809
+    PriorityQueue<Integer> queue=new PriorityQueue<>();
+    //System.out.println("queue"+queue);
+    for(int i:nums){
+       // System.out.print("i "+i);
+        queue.add(i);
+        //System.out.print("queue add :"+queue);
+        if(queue.size()>k){
+            //System.out.print("size :"+queue.size());
+            queue.remove();
+
+           // System.out.print("queue remove :"+queue.remove());
+        }
+    } 
+
+    return queue.remove();
+
+
         
     }
 }
